1685b6be55abb621ace4a1a82f2600f11d20fb51
jennifer-blip <jodde@student.42.fr>
2026-07-16 21:15:57 +0200

    Add files via upload

--- module04/ex00/FileLoader.py
+++ module04/ex00/FileLoader.py
@@ -8,8 +8,12 @@ class FileLoader:
 		"""takes as an argument the file path of the dataset to load,
 		displays a message specifying the dimensions of the dataset (e.g. 340 x 500) and
 		returns the dataset loaded as a pandas.DataFrame."""
+		data = pd.read_csv(path, sep=';')
+		print(f"Loading dataset of dimensions {data.shape[0]} x {data.shape[1]}")
+		return data
 		
 	def display(self, df, n): 
 		"""takes a pandas.DataFrame and an integer as arguments,
 		displays the first n rows of the dataset if n is positive, or the last n rows if n is
 		negative."""
+		print(df.head(n))
